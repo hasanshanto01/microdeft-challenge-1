b10136e9a086461e4a8e8f43025ce2b0e7fc5a54
hasanshanto01 <hasanshanto01@gmail.com>
2023-06-04 11:47:38 +0600

    sort fn added

--- src/Pages/Home/FilterBar/FilterBar.js
+++ src/Pages/Home/FilterBar/FilterBar.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const FilterBar = () => {
+
+    const handleSorting = (e) => {
+
+        //console.log(e.target.value);
+        const sortOrder = e.target.value;
+
+        fetch(`https://fakestoreapi.com/products?sort=${sortOrder}`)
+            .then(res => res.json())
+            .then(json => console.log(json))
+
+    }
+
     return (
         <div className='flex gap-2 mb-5'>
             <div className='flex gap-x-1 w-4/6'>
@@ -8,10 +20,10 @@ const FilterBar = () => {
                 <input type="text" placeholder="Filter products" className="input input-bordered w-full" />
             </div>
             <div className='w-2/6'>
-                <select className="select select-bordered w-full">
-                    <option>Nothing</option>
-                    <option value=''>Price: Low to High</option>
-                    <option value=''>Price: High to Low</option>
+                <select className="select select-bordered w-full" onChange={handleSorting}>
+                    <option value>Nothing</option>
+                    <option value='asc'>Price: Low to High</option>
+                    <option value='desc'>Price: High to Low</option>
                 </select>
             </div>
         </div>
